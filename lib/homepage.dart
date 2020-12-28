a5a9ff45c12918c970956683b621b892b264ec99
akshay-0505 <akshaygadhave531@gmail.com>
2020-12-28 22:31:24 +0530

    Done till Deleting tasks form list

---
+++ lib/homepage.dart
@@ -0,0 +1,143 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class ListItem {
+  bool _value = false;
+  String task;
+  ListItem(String task) {
+    this.task = task;
+  }
+}
+
+class _homepageState extends State<homepage> {
+  List<ListItem> tasks = List();
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController tcontrol = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo-App"),
+      ),
+      body: tasks.length == 0
+          ? Center(
+              child: Text("No Task to do"),
+            )
+          : ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Container(
+                    child: ListTile(
+                      title: Row(
+                        children: <Widget>[
+                          Checkbox(
+                            value: tasks[index]._value,
+                            onChanged: (bool value) {
+                              setState(() {
+                                tasks[index]._value = value;
+                              });
+                            },
+                          ),
+                          new Expanded(child: Text(tasks[index].task)),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        color: Colors.red,
+                        onPressed: () {
+                          setState(() {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [Text("Are you sure?")],
+                                    ),
+                                    actions: [
+                                       RaisedButton(
+                                          child: Text("No"),
+                                          onPressed: () {
+                                            setState(() {
+                                              
+                                            });
+                                            Navigator.of(context).pop();
+                                          },color: Colors.green,
+                                          ),
+                                      RaisedButton(
+                                          child: Text("Yes"),
+                                          onPressed: () {
+                                            setState(() {
+                                              tasks.removeAt(index);
+                                            });
+                                            Navigator.of(context).pop();
+                                          },color: Colors.red,),
+                                    ],
+                                  );
+                                });
+                          });
+                        },
+                        icon: Icon(Icons.delete),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Container(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextField(
+                            controller: tcontrol,
+                          ),
+                        ],
+                      ),
+                    ),
+                    actions: [
+                      RaisedButton(
+                        child: Text("Add Task"),
+                        onPressed: () {
+                          setState(() {
+                            tasks.add(new ListItem(tcontrol.text));
+                          });
+                          Navigator.of(context).pop();
+                        },
+                        color: Colors.green,
+                      ),    
+                      RaisedButton(
+                        child: Text("Close"),
+                        onPressed: () {
+                          setState(() {
+                            
+                          });
+                          Navigator.of(context).pop();
+                        },
+                        color: Colors.blue,
+                      )
+                    ],
+                  );
+                });
+          }),
+    );
+  }
+
+  Checkbox newMethod() => Checkbox(
+        onChanged: (bool value) {},
+        value: null,
+      );
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+import 'package:todoapp/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Todo-App",
+      home: homepage(),
+   
+    );
+  }
+}
